076b6a4bdaf8c525dad3fbc39dc27e42598993e6
MARVIN <o.silverygold@gmail.com>
2024-07-24 20:39:53 +0300

    Add basic app layout

---
+++ app/index.jsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-2xl font-pblack">Aora!</Text>
+      <Link href='/profile'>Go to Profile</Link>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ app/profile.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PROFILE = () => {
+  return (
+    <View>
+      <Text>PROFILE</Text>
+    </View>
+  )
+}
+
+export default PROFILE
+
+const styles = StyleSheet.create({})
