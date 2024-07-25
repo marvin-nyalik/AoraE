ad201dc1375f073e775df3b1bfbfb9da33a7550c
MARVIN <o.silverygold@gmail.com>
2024-07-25 16:26:52 +0300

    Create updated landing page

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { StatusBar } from "expo-status-bar";
+import { Text, View, ScrollView, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "../constants/images";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl font-pblack">Aora!</Text>
-      <Link href='/profile'>Go to Profile</Link>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full h-screen justify-center items-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="w-full h-[300px] max-w-[380px]"
+            resizeMode="contain"
+          />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+          <View className="relative mt-5">
+            <Text className="text-white text-3xl font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+          </View>
+
+          <Image
+            source={images.path}
+            className="absolute-bottom-2 w-[136px] h-[15px]"
+            resizeMode="contain"
+          />
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: Embark on a journey of limitless
+            exploration
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => {}}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light"/>
+    </SafeAreaView>
+  )
+}
