d08b9061d0e3ee53fcb182c510865896c4ce376f
MARVIN <o.silverygold@gmail.com>
2024-07-29 21:48:18 +0300

    Implement play video functionality

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,6 +1,7 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import { icons } from "../constants";
+import { Video, ResizeMode } from "expo-av";
 
 const VideoCard = ({
   video: {
@@ -45,7 +46,18 @@ const VideoCard = ({
       </View>
 
       {play ? (
-        <Text>Playing</Text>
+        <Video
+          source={{ uri: video }}
+          className="w-full h-60 rounded-xl mt-3"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
       ) : (
         <TouchableOpacity
           onPress={() => setPlay(true)}
